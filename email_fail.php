acd61b4e5ebf3f77eaa6ad2fbe072d5aa0c9dcc3
Fiodor <fiodortonti@Fiodors-MacBook-Air.local>
2015-10-29 15:51:43 +0100

    added email contact

--- email_fail.php
+++ email_fail.php
@@ -1,3 +1,4 @@
-<p class="lead">We didn't recognise this email address. Please try a different one or get in touch at [contact info here]</p>
+
+<p class="lead">We didn't recognise this email address. Please try a different one or get in touch at <a href="mailto:tomasz.s@numa.co">tomasz.s@numa.co</a></p>
 <br>
-<button class="btn btn-primary" onclick="history.go(-1);">Back</button>
+<button class="btn btn-primary btn-lg" onclick="history.go(-1);">Back</button></div>
