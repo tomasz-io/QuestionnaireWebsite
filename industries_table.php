374f5e2389bfa5125d01d2e023d484a6ab8f142b
Fiodor <fiodortonti@Fiodors-MacBook-Air.local>
2015-10-29 14:31:44 +0100

    Fixed label issue on table, added jumbotron style

--- industries_table.php
+++ industries_table.php
@@ -2,39 +2,39 @@
   <p class="lead">What are your main industries?</p>
   <table class="table" id="table">
     <tr>
-      <td><input type="checkbox" name="industry[]" value="art & design"/> Art & Design</td>
-      <td><input type="checkbox" name="industry[]" value="charity & non-profit"/> Charity & non-profit</td>
-      <td><input type="checkbox" name="industry[]" value="consumer goods"/> Consumer goods</td>
+      <td><input type="checkbox" id="artdesign" value="art & design"/> <label for="artdesign">Art & Design</label></td>
+      <td><input type="checkbox" id="charity" value="charity & non-profit"/> <label for="charity">Charity & non-profit</label></td>
+      <td><input type="checkbox" id="consumergoods" value="consumer goods"/> <label for="consumergoods">Consumer goods</label></td>
     </tr>
     <tr>
-      <td><input type="checkbox" name="industry[]" value="education"/> Education</td>
-      <td><input type="checkbox" name="industry[]" value="energy"/> Energy</td>
-      <td><input type="checkbox" name="industry[]" value="family & home care"/> Family & home care</td>
+      <td><input type="checkbox" id="education" value="education"/> <label for="education">Education</label></td>
+      <td><input type="checkbox" id="energy" value="energy"/> <label for="energy">Energy</label></td>
+      <td><input type="checkbox" id="family" value="family & home care"/> <label for="family">Family & home care</label></td>
     </tr>
     <tr>
-      <td><input type="checkbox" name="industry[]" value="fashion & textile"/> Fashion & textile</td>
-      <td><input type="checkbox" name="industry[]" value="financial services"/> Financial services</td>
-      <td><input type="checkbox" name="industry[]" value="food/beverages/tobacco"/> Food/beverages/tobacco</td>
+      <td><input type="checkbox" id="fashion" value="fashion & textile"/> <label for="fashion">Fashion & textile</label></td>
+      <td><input type="checkbox" id="financial" value="financial services"/> <label for="financial">Financial services</label></td>
+      <td><input type="checkbox" id="food" value="food/beverages/tobacco"/> <label for="food">Food/beverages/tobacco</label></td>
     </tr>
     <tr>
-      <td><input type="checkbox" name="industry[]" value="healthcare"/> Healthcare</td>
-      <td><input type="checkbox" name="industry[]" value="lifestyle & leisure"/> Lifestyle & leisure</td>
-      <td><input type="checkbox" name="industry[]" value="marketing & communication"/> Marketing & communication</td>
+      <td><input type="checkbox" id="healthcare" value="healthcare"/> <label for="healthcare">Healthcare</label></td>
+      <td><input type="checkbox" id="lifestyle" value="lifestyle & leisure"/> <label for="lifestyle">Lifestyle & leisure</label></td>
+      <td><input type="checkbox" id="marketing" value="marketing & communication"/> <label for="marketing">Marketing & communication</label></td>
     </tr>
     <tr>
-      <td><input type="checkbox" name="industry[]" value="media & entertainment"/> Media & entertainment</td>
-      <td><input type="checkbox" name="industry[]" value="professional services"/> Professional services</td>
-      <td><input type="checkbox" name="industry[]" value="security"/> Security</td>
+      <td><input type="checkbox" id="media" value="media & entertainment"/> <label for="media">Media & entertainment</label></td>
+      <td><input type="checkbox" id="professionalservices" value="professional services"/> <label for="professionalservices">Professional services</label></td>
+      <td><input type="checkbox" id="security" value="security"/> <label for="security">Security</label></td>
     </tr>
     <tr>
-      <td><input type="checkbox" name="industry[]" value="semiconductors"/> Semiconductors</td>
-      <td><input type="checkbox" name="industry[]" value="software"/> Software</td>
-      <td><input type="checkbox" name="industry[]" value="tech hardware"/> Tech hardware</td>
+      <td><input type="checkbox" id="semiconductors" value="semiconductors"/> <label for="semiconductors">Semiconductors</label></td>
+      <td><input type="checkbox" id="software" value="software"/> <label for="software">Software</label></td>
+      <td><input type="checkbox" id="techhardware" value="tech hardware"/> <label for="techhardware">Tech hardware</label></td>
     </tr>
     <tr>
-      <td><input type="checkbox" name="industry[]" value="telecommunications"/> Telecommunications</td>
-      <td><input type="checkbox" name="industry[]" value="transport/logistics./mobility"/> Transport/logistics/mobility</td>
-      <td><input type="checkbox" name="industry[]" value="travel & tourism"/> Travel & tourism</td>
+      <td><input type="checkbox" id="telecommunications" value="telecommunications"/> <label for="telecommunications">Telecommunications</label></td>
+      <td><input type="checkbox" id="transport" value="transport/logistics./mobility"/> <label for="transport">Transport/logistics/mobility</label></td>
+      <td><input type="checkbox" id="travel" value="travel & tourism"/> <label for="travel">Travel & tourism</label></td>
     </tr>
   </table>
   <br>
