962add4224c61050cf3456dd670fb80fd4675bc3
Fiodor <fiodortonti@Fiodors-MacBook-Air.local>
2015-11-23 17:45:07 +0100

    Minor text update

--- email_fail.php
+++ email_fail.php
@@ -38,7 +38,7 @@
     <div class="container jumbotron">
         <div class="row">
             <div class="col-lg-12 text-center">
-              <p class="lead">We didn't recognise this email address. Please try a different one or get in touch with <a href="mailto:tomasz.s@numa.co">tomasz.s@numa.co</a></p>
+              <p class="lead">We didn't recognise this email address. <br> Please try a different one or get in touch with <a href="mailto:tomasz.s@numa.co">tomasz.s@numa.co</a></p>
               <br>
               <button class="btn btn-primary btn-lg" onclick="history.go(-1);">Back</button></div>
             </div>
